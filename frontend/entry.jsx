60c94bd584b7e536440b77ce1c1d40106c32c444
Young Chul Lim <youngchullim95@gmail.com>
2019-02-26 17:25:03 -0800

    Add UserAuth Store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 // TEST
 import {login, logout, signup} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  
+  const store = configureStore();
   // TEST
   window.login = login;
   window.logout = logout;
   window.signup = signup;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   
   ReactDOM.render(<h1>Welcome to Modify</h1>, root);
 });
