d71dd83dc7e4a268be80cfaaddae3ae553ca32b6
Young Chul Lim <youngchullim95@gmail.com>
2019-03-01 14:21:23 -0800

    Remove sprockets

---
+++ app/controllers/api/playlists_controller.rb
@@ -0,0 +1,38 @@
+class Api::PlaylistsController < ApplicationController
+  before_action :require_login
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def index
+    @playlists = Playlist.all
+  end
+
+  def create
+    @playlist = Playlist.new(playlist_params)
+
+    render :show if @playlist.save
+  end
+
+  def show
+    @playlist.find_by(id: params[:id])
+  end
+
+  def update
+    @playlist = Playlist.find_by(id: params[:id])
+    render :show if @playlist.update_attritbutes(playlist_params)
+  end
+
+  def destroy
+    @playlist = Playlist.find_by(id: params[:id])
+    @playlist.destroy
+    render :index
+  end
+
+  private
+  def playlist_params
+    params.require(:playlist).permit(:name)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :users, only: [:show, :create]
     resource :session, only: [:destroy, :create]
     resources :songs, only: [:index, :show]
+    resources :playlists, only: [:index, :create, :show, :update, :destroy]
     end
 
   root to: 'static_pages#root'
