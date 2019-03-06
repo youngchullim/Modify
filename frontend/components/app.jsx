9e1f52fe79ac59a7dc71a2427cf2192bb4693225
Young Chul Lim <youngchullim95@gmail.com>
2019-03-05 19:45:46 -0800

    Add Modal to App Component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,13 +5,15 @@ import { AuthRoute, ProtectedRoute }from '../util/route_util';
 import GreetingContainer from './greeting/greeting_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
-
+import Modal from './modal/modal';
 import UnauthComponent from './unauth';
 import AuthComponent from './auth';
 
 const App = () => {
   return (
     <div>
+      <Modal />
+      
       <Switch>
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
