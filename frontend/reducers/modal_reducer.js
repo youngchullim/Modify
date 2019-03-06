6a89832cb7aa62a7e5752f379dd7ef93a6269afe
Young Chul Lim <youngchullim95@gmail.com>
2019-03-05 19:44:20 -0800

    Add Modal Reducer

---
+++ frontend/reducers/modal_reducer.js
@@ -0,0 +1,17 @@
+import { OPEN_MODAL, CLOSE_MODAL } from '../actions/modal_actions';
+
+const modalReducer = (oldState=null, action) => {
+  Object.freeze(oldState);
+  let state;
+
+  switch(action.type) {
+    case OPEN_MODAL:
+      return action.modal;
+    case CLOSE_MODAL:
+      return null;
+    default:
+      return oldState;
+  }
+}
+
+export default modalReducer;
