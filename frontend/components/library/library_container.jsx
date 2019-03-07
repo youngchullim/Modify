43959032d17f59d595cb996f906e3e9855b80223
Young Chul Lim <youngchullim95@gmail.com>
2019-03-07 08:54:27 -0800

    Add more to Library Container

--- frontend/components/library/library_container.jsx
+++ frontend/components/library/library_container.jsx
@@ -13,10 +13,12 @@ import {
   deletePlaylist 
   } from '../../actions/playlist_actions';
 
+import { fetchSong, fetchSongs } from '../../actions/song_actions';
+
 const mapStateToProps = state => {
   return({
-    user: state.entities.users[state.session.id]
-    // playlist: state.entities.playlists[playlist.id]
+    user: state.entities.users[state.session.id],
+    playlists: Object.values(state.entities.playlists)
   });
 };
 
@@ -29,21 +31,17 @@ const mapDispatchToProps = dispatch => {
           <div className="playlist-center">NEW PLAYLIST</div>
         </div>
       </div>
-
-      // <button onClick={ () => dispatch(openModal('create'))}>
-      //   Create
-      // </button>
     ),
-    closeModal: () => dispatch(closeModal())
+    closeModal: () => dispatch(closeModal()),
+    fetchPlaylists: () => dispatch(fetchPlaylists()),
+    fetchPlaylist: (id) => dispatch(fetchPlaylist(id)),
+    createPlaylist: (playlist) => dispatch(createPlaylist(playlist)),
+    updatePlaylist: (playlist) => dispatch(updatePlaylist(playlist)),
+    deletePlaylist: (id) => dispatch(deletePlaylist(id)),
+
+    fetchSongs: () => dispatch(fetchSongs()),
+    fetchSong: (id) => dispatch(fetchSong(id))
   });
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Library);
-
-    // @@@@@@@@@@@     START HERE  (MDP)    @@@@@@@@@@@@@@
-    // how do I get :id and :playlist
-    // fetchPlaylists: () => dispatch(fetchPlaylists()),
-    // fetchPlaylist: (id) => dispatch(fetchPlaylist(id)),
-    // createPlaylist: (playlist) => dispatch(createPlaylist(playlist)),
-    // updatePlaylist: (playlist) => dispatch(updatePlaylist(playlist)),
-    // deletePlaylist: (id) => dispatch(deletePlaylist(id))
