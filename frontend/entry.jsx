8cb6c9075026dfef907fd310b2a9dc033d9a12e3
Young Chul Lim <youngchullim95@gmail.com>
2019-03-07 08:54:57 -0800

    Remove Test Code from Entry

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -4,16 +4,13 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 // TEST
-// import {login, logout, signup} from './util/session_api_util';
-import { login, logout, signup } from './actions/session_actions';
-import { fetchSong } from './util/song_api_util';
+// import { login, logout, signup } from './actions/session_actions';
+// import { fetchSongs } from './actions/song_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
 
-  
   let store;
   if (window.user) {
     const preloadedState = {
@@ -29,12 +26,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // TEST
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchSong = fetchSong;
+  // window.login = login;
+  // window.logout = logout;
+  // window.signup = signup;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.fetchSongs = fetchSongs;
+  // window.store = store;
 
   ReactDOM.render(<Root store={store}/>, root);
 });
