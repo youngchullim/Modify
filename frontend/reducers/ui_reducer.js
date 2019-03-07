a1f707759bff632b1a0663824b6bb9af3b5c4f9c
Young Chul Lim <youngchullim95@gmail.com>
2019-03-07 08:55:32 -0800

    Add Music Reducer to UI Reducer

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import modal from './modal_reducer';
+import music from './music_reducer';
 
 export default combineReducers({
-  modal
+  modal,
+  music
 });
