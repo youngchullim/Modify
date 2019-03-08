bbe3ba80288e7f8ad817d7cfc90beac78a2fa9a3
Young Chul Lim <youngchullim95@gmail.com>
2019-03-08 10:39:47 -0800

    Add implicit return

--- frontend/components/playlist/playlist.jsx
+++ frontend/components/playlist/playlist.jsx
@@ -12,13 +12,14 @@ class Playlist extends React.Component {
     return(
       <div className="all-playlists">
         <ul className="ul-playlist">
-          {this.props.playlists.map ( (playlist, idx) => 
+                  {/* ADD implicit return () */}
+          {this.props.playlists.map ( (playlist, idx) => (
           <li key={idx} className="li-playlist">
             <NavLink to={`/library/${playlist.id}`}>
               <div className="playlist-photo"></div>
               <div className="playlist-name">{playlist.name}</div>
             </NavLink>
-          </li>)}
+          </li>))}
         </ul>
       </div>
     )
