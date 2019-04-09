7d6a0c8d499561acc9eac7c2f72682d5ab351ba2
Young Chul Lim <youngchullim95@gmail.com>
2019-04-08 17:43:23 -0700

    Add SearchResult skeleton

---
+++ frontend/components/search/search_result.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProtectedRoute } from '../../util/route_util';
+import { withRouter, NavLink, Route } from 'react-router-dom';
+
+class SearchResult extends React.Component{
+  render() {
+  }
+}
+
+export default withRouter(SearchResult);
