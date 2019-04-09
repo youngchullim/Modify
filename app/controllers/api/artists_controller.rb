7b8494cf414de93aa3ceff0564e93b3e776aa4aa
Young Chul Lim <youngchullim95@gmail.com>
2019-04-08 17:46:02 -0700

    Add Artist Controller

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,2 +1,16 @@
 class Api::ArtistsController < ApplicationController
+  before_action :require_login
+
+  def show
+    @artist = Artist.find_by(id: params[:id])
+  end
+
+  def index
+    @artists = Artist.all
+  end
+
+  private
+  def artist_parans
+    params.require(:artist).permit(:name, :bio)
+  end
 end
