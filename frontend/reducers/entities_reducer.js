3674ada552e8e6078ba0fe98c60a4b5f40b0d7e8
Young Chul Lim <youngchullim95@gmail.com>
2019-04-10 14:34:13 -0700

    Add Artists to EntitiesReducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import users from './users_reducer';
 import songs from './songs_reducer';
 import playlists from './playlists_reducer';
 import albums from './albums_reducer';
+import artists from './artists_reducer';
 
 const entitiesReducer = combineReducers({
   users,
   songs,
   playlists,
-  albums
+  albums,
+  artists
 });
 
 export default entitiesReducer;
