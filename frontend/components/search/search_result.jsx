dc3df0cb25d743b3c1f16fffec83c63cc1405b94
Young Chul Lim <youngchullim95@gmail.com>
2019-04-11 15:09:12 -0700

    Add SearchResultContainer

--- frontend/components/search/search_result.jsx
+++ frontend/components/search/search_result.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { ProtectedRoute } from '../../util/route_util';
 import { withRouter, NavLink, Route } from 'react-router-dom';
 
+import ArtistContainer from '../artist/artist_container';
+import AlbumContainer from '../album/album_container';
+import PlaylistContainer from '../playlist/playlist_container';
+
 class SearchResult extends React.Component{
+  constructor(props) {
+    super(props);
+  }
   render() {
+
+    return (
+      <div>
+        SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST 
+        SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST 
+        SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST SEARCHTEST 
+      </div>
+    )
   }
 }
 
