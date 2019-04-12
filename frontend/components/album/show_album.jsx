422ee5ebaaa02c2f37b71502b5b191fd05a044dd
Young Chul Lim <youngchullim95@gmail.com>
2019-04-11 20:46:35 -0700

    Update AlbumShowInfo div

--- frontend/components/album/show_album.jsx
+++ frontend/components/album/show_album.jsx
@@ -26,11 +26,11 @@ class ShowAlbum extends React.Component {
           <NavLink className="albumshow-artistname-link" to={`/artists/${this.props.album.artist.id}`}>
             <span className="albumshow-artistname">{this.props.album.artist.name}</span>
           </NavLink> 
-          <button className="album-play">Play</button>
+          <button className="album-play">PLAY</button>
           <div className="albumshow-info">
             <span className="album-year">{this.props.album.year}</span>
-            <span className="split-dot">.</span>
-            <span className="album-length">{this.props.album.songs.length}</span>
+            <span className="album-split-dot">.</span>
+            <span className="album-length">{this.props.album.songs.length} SONGS</span>
           </div>
 
         </div>
