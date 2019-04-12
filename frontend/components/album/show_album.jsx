b736eeb45342e76a7ad6c08240b7d3d2f48b76f2
Young Chul Lim <youngchullim95@gmail.com>
2019-04-11 18:36:32 -0700

    Add basic ShowAlbum Component

--- frontend/components/album/show_album.jsx
+++ frontend/components/album/show_album.jsx
@@ -16,12 +16,17 @@ class ShowAlbum extends React.Component {
       return null;
     }
     return(
-      <div>
-        <h1>{this.props.album.title}</h1>
-        <div>ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM
-        ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM
-        ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM
-        ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM ALBUM
+      <div className="albumshow-page">
+        {/* <h1>{this.props.album.title}</h1> */}
+        <div>
+          <img className="albumshow-photo" src={this.props.album.photo} />
+          <ul className="ul-album-songs">
+            {this.props.album.songs.map( (song, idx) => (
+              <li className="li-album-song" key={idx}>
+                <div>{song.title}</div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     )
