8c8c14c143a82660f85036c7d82d44a814d04688
Young Chul Lim <youngchullim95@gmail.com>
2019-04-15 20:03:00 -0700

    Add ArtistsUser Controller

--- app/controllers/api/artists_user_controller.rb
+++ app/controllers/api/artists_user_controller.rb
@@ -1,2 +1,30 @@
 class Api::ArtistsUserController < ApplicationController
+  def index 
+    @user = User.find_by(id: params[:user_id])
+    @artists_users = @user.artists_users.map { |artist| Artist.find_by(id: artist.artist_id) }
+    render :index
+  end
+
+  def create
+    @artists_user = ArtistsUser.new(artists_user_params)
+    if @artists_user.save
+      @user = User.find_by(id: @artists_user.user_id)
+      render json: ["Saved to Library"], status: 200
+    else
+      render json: ["Unable to Save to Library"], status: 404
+    end
+  end
+
+  def destroy
+    @artists_user = ArtistsUser.find_by(id: params[:id])
+    @user = User.find_by(id: @artists_user.user_id)
+
+    @artists_user.destroy
+    render json: ["Removed from Library"], status: 200
+  end
+
+  private
+  def artists_user_params 
+    params.require(:artists_user).permit(:artist_id, :user_id)
+  end
 end
