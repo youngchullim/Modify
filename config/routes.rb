38defca5a3cae5cee5a0cf11ea1519cc82ee55db
Young Chul Lim <youngchullim95@gmail.com>
2019-04-15 17:51:32 -0700

    Add AlbumsUser routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :albums, only: [:show, :index]
     resources :artists, only: [:show, :index]
     resources :songs_user, only: [:index, :create, :destroy]
+    resources :albums_user, only: [:index, :create, :destroy]
     end
 
   root to: 'static_pages#root'
