e19d21df18102a62ae92e27da3c967b01cfedc1c
Young Chul Lim <youngchullim95@gmail.com>
2019-04-15 18:21:40 -0700

    Add AlbumsUser Controller

--- app/controllers/api/albums_user_controller.rb
+++ app/controllers/api/albums_user_controller.rb
@@ -1,12 +1,26 @@
 class Api::AlbumsUserController < ApplicationController
-  def index
-    
+  def index 
+    @user = User.find_by(id: params[:user_id])
+    @albums_users = @user.albums_users.map { |album| Album.find_by(id: album.album_id) }
+    render :index
   end
 
   def create
+    @albums_user = AlbumsUser.new(albums_user_params)
+    if @albums_user.save
+      @user = User.find_by(id: @albums_user.user_id)
+      render json: ["Saved to Library"], status: 200
+    else
+      render json: ["Unable to Save to Library"], status: 404
+    end
   end
 
   def destroy
+    @albums_user = AlbumsUser.find_by(id: params[:id])
+    @user = User.find_by(id: @albums_user.user_id)
+
+    @albums_user.destroy
+    render json: ["Removed from Library"], status: 200
   end
 
   private
