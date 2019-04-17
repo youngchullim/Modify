f87aa6a0eab1903c8b01eb04c28068688733ff56
Young Chul Lim <youngchullim95@gmail.com>
2019-04-17 13:36:40 -0700

    Add SongsUser ApiUtil

---
+++ frontend/util/songs_user_api_util.js
@@ -0,0 +1,21 @@
+export const createSongsUser = (songsUser) => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/songs_users",
+    data: { songsUser }
+  });
+};
+
+export const deleteSongsUser = (id) => {
+  return $.ajax({
+    method: "DELETE",
+    url: `/api/songs_users/${id}`
+  });
+};
+
+export const fetchSongsUsers = (id) => {
+  return $.ajax({
+    method: "GET",
+    url: `/api/users/${id}/songs_users`
+  });
+};
