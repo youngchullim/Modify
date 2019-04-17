0ffa9019d6bf19600ba2bbe2cc8915d338be29be
Young Chul Lim <youngchullim95@gmail.com>
2019-04-17 14:12:34 -0700

    Update AlbumActions

--- frontend/actions/album_actions.js
+++ frontend/actions/album_actions.js
@@ -1,7 +1,10 @@
 import * as AlbumApiUtil from '../util/album_api_util';
+import * as AlbumsUserApiUtil from '../util/albums_user_api_util';
 
 export const RECEIVE_ALBUMS = "RECEIVE_ALBUMS";
 export const RECEIVE_ALBUM = "RECEIVE_ALBUM";
+export const RECEIVE_ALBUMS_USER = "RECEIVE_ALBUMS_USER";
+export const REMOVE_ALBUMS = "REMOVE_ALBUMS";
 
 export const receiveAlbums = (albums) => {
   return({
@@ -27,3 +30,30 @@ export const fetchAlbum = (id) => (dispatch) => (
     .then( album => dispatch(receiveAlbum(album)))
 );
 
+export const receiveAlbumsUser = user => {
+  return({
+    type: RECEIVE_ALBUMS_USER,
+    user
+  });
+};
+
+export const removeAlbums = () => {
+  return({
+    type: REMOVE_ALBUMS
+  });
+};
+
+export const createAlbumsUser = (user_id, album_id) => (dispatch) => (
+  AlbumsUserApiUtil.createAlbumsUser({user_id, album_id})
+    .then( user => dispatch(receiveAlbumsUser(user)))
+);
+
+export const deleteAlbumsUser = (id) => (dispatch) => (
+  AlbumsUserApiUtil.deleteAlbumsUser(id)
+    .then( user => dispatch(receiveAlbumsUser(user)))
+);
+
+export const fetchAlbumsUsers = (id) => (dispatch) => (
+  AlbumsUserApiUtil.fetchAlbumsUsers(id)
+    .then( albums => dispatch(receiveAlbums(albums)))
+);
