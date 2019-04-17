6016ca1ad3d0c423aecaea42c0da4e995d7d7053
Young Chul Lim <youngchullim95@gmail.com>
2019-04-17 15:11:55 -0700

    Update PlaylistActions

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,8 +1,10 @@
 import * as PlaylistApiUtil from '../util/playlist_api_util';
+import * as PlaylistsUserApiUtil from '../util/playlists_user_api_util';
 
 export const RECEIVE_PLAYLISTS = "RECEIVE_PLAYLISTS";
 export const RECEIVE_PLAYLIST = "RECEIVE_PLAYLIST";
 export const REMOVE_PLAYLIST = "REMOVE_PLAYLIST";
+// export const RECEIVE_PLAYLISTS_USER = "RECEIVE_PLAYLISTS_USER";
 
 export const receivePlaylists = (playlists) => {
   return({
@@ -48,4 +50,16 @@ export const updatePlaylist = (playlist) => (dispatch) => (
 export const deletePlaylist = (id) => (dispatch) => (
   PlaylistApiUtil.deletePlaylist(id)
     .then( playlist => dispatch(removePlaylist(playlist)))
+);
+
+// export const receivePlaylistsUser = user => {
+//   return({
+//     type: RECEIVE_PLAYLISTS_USER,
+//     user
+//   });
+// };
+
+export const fetchPlaylistsUsers = (id) => (dispatch) => (
+  PlaylistsUserApiUtil.fetchPlaylistsUsers(id)
+    .then( playlists => dispatch(receivePlaylists(playlists)))
 );
