b1991632522ba36f4d18832af463856dae4e9e48
Young Chul Lim <youngchullim95@gmail.com>
2019-04-17 14:27:26 -0700

    Update SongActions

--- frontend/actions/song_actions.js
+++ frontend/actions/song_actions.js
@@ -1,7 +1,10 @@
 import * as SongApiUtil from '../util/song_api_util';
+import * as SongsUserApiUtil from '../util/songs_user_api_util';
 
 export const RECEIVE_SONGS = "RECEIVE_SONGS";
 export const RECEIVE_SONG = "RECEIVE_SONG";
+export const RECEIVE_SONGS_USER = "RECEIVE_SONGS_USER";
+export const REMOVE_SONG = "REMOVE_SONG";
 
 export const receiveSongs = (songs) => {
   return({
@@ -27,3 +30,30 @@ export const fetchSong = (id) => (dispatch) => (
     .then( song => dispatch(receiveSong(song)))
 );
 
+export const receiveSongsUser = user => {
+  return({
+    type: RECEIVE_SONGS_USER,
+    user
+  });
+};
+
+export const removeSong = () => {
+  return({
+    type: REMOVE_SONG
+  });
+};
+
+export const createSongsUser = (user_id, song_id) => (dispatch) => (
+  SongsUserApiUtil.createSongsUser({user_id, song_id})
+    .then( user => dispatch(receiveSongsUser(user)))
+);
+
+export const deleteSongsUser = (id) = (dispatch) => (
+  SongsUserApiUtil.deleteSongsUser(id)
+    .then( user => dispatch(receiveSongsUser(user)))
+);
+
+export const fetchSongsUsers = (id) => (dispatch) => (
+  SongsUserApiUtil.fetchSongsUsers(id)
+    .then( songs => dispatch(receiveSongs(songs)))
+);
