8a87d1c24c4bb56b31b9f2239c037e54c2bf262e
Young Chul Lim <youngchullim95@gmail.com>
2019-04-17 14:21:37 -0700

    Update ArtistActions

--- frontend/actions/artist_actions.js
+++ frontend/actions/artist_actions.js
@@ -1,7 +1,10 @@
 import * as ArtistApiUtil from '../util/artist_api_util';
+import * as ArtistsUserApiUtil from '../util/artists_user_api_util';
 
 export const RECEIVE_ARTISTS = "RECEIVE_ARTISTS";
 export const RECEIVE_ARTIST = "RECEIVE_ARTIST";
+export const RECEIVE_ARTISTS_USER = "RECEIVE_ARTISTS_USER";
+export const REMOVE_ARTIST = "REMOVE_ARTIST";
 
 export const receiveArtists = (artists) => {
   return({
@@ -27,3 +30,30 @@ export const fetchArtist = (id) => (dispatch) => (
     .then( artist => dispatch(receiveArtist(artist)))
 );
 
+export const receiveArtistsUser = user => {
+  return({
+    type: RECEIVE_ARTISTS_USER,
+    user
+  });
+};
+
+export const removeArtist = () => {
+  return({
+    type: REMOVE_ARTIST
+  });
+};
+
+export const createArtistsUser = (user_id, artist_id) => (dispatch) => (
+  ArtistsUserApiUtil.createArtistsUser({user_id, artist_id})
+    .then( user => dispatch(receiveArtistsUser(user)))
+);
+
+export const deleteArtistsUser = (id) => (dispatch) => (
+  ArtistsUserApiUtil.deleteArtistsUser(id)
+    .then( user => dispatch(receiveArtistsUser(user)))
+);
+
+export const fetchArtistsUsers = (id) => (dispatch) => (
+  ArtistsUserApiUtil.fetchArtistsUsers(id)
+    .then( artists => dispatch(receiveArtists(artists)))
+);
