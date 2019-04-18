676ca4dc191e44a20780b594aae655eb527b16b6
Young Chul Lim <youngchullim95@gmail.com>
2019-04-18 11:18:21 -0700

    Update SongActions

--- frontend/actions/song_actions.js
+++ frontend/actions/song_actions.js
@@ -1,10 +1,13 @@
 import * as SongApiUtil from '../util/song_api_util';
 import * as SongsUserApiUtil from '../util/songs_user_api_util';
+import * as PlaylistsSongApiUtil from '../util/playlists_song_api_util';
+import { receivePlaylist } from './playlist_actions';
 
 export const RECEIVE_SONGS = "RECEIVE_SONGS";
 export const RECEIVE_SONG = "RECEIVE_SONG";
 export const RECEIVE_SONGS_USER = "RECEIVE_SONGS_USER";
 export const REMOVE_SONG = "REMOVE_SONG";
+export const RECEIVE_PLAYLISTS_SONG = "RECEIVE_PLAYLISTS_SONG";
 
 export const receiveSongs = (songs) => {
   return({
@@ -56,4 +59,14 @@ export const deleteSongsUser = (id) = (dispatch) => (
 export const fetchSongsUsers = (id) => (dispatch) => (
   SongsUserApiUtil.fetchSongsUsers(id)
     .then( songs => dispatch(receiveSongs(songs)))
+);
+
+export const createPlaylistsSong = (playlist_id, song_id) => (dispatch) => (
+  PlaylistsSongApiUtil.createPlaylistsSong({playlist_id, song_id})
+    .then( playlist => dispatch(receivePlaylist(playlist)))
+);
+
+export const deletePlaylistsSong = (id) => (dispatch) => (
+  PlaylistsSongApiUtil.deletePlaylistsSong(id)
+    .then( playlist => dispatch(receivePlaylist(playlist)))
 );
