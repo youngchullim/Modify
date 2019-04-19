2e836eecfd1892564f7d27052a4255cf1c54987a
Young Chul Lim <youngchullim95@gmail.com>
2019-04-19 15:34:00 -0700

    Add songs albums artists to UsersReducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,4 +1,8 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_SONGS_USER } from '../actions/song_actions';
+import { RECEIVE_ALBUMS_USER } from '../actions/album_actions';
+import { RECEIVE_ARTISTS_USER } from '../actions/artist_actions';
+
 import merge from 'lodash/merge';
 
 const usersReducer = (oldState={}, action) => {
@@ -7,6 +11,9 @@ const usersReducer = (oldState={}, action) => {
 
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
+    case RECEIVE_SONGS_USER:
+    case RECEIVE_ALBUMS_USER:
+    case RECEIVE_ARTISTS_USER:
       state = merge({}, oldState, {[action.user.id]: action.user});
       return state;
     default:
