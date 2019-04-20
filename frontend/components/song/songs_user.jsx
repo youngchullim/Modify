147432b9f281c0a8c3f6cae2d51cec748b1c26a4
Young Chul Lim <youngchullim95@gmail.com>
2019-04-19 20:36:01 -0700

    Add SongsUserComponent skeleton

---
+++ frontend/components/song/songs_user.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { button, Link, NavLink } from 'react-router-dom';
+
+class SongsUser extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+    this.props.fetchSongsUsers(this.props.user.id);
+  }
+
+  render() {
+    return(
+      <div className="songs-component">
+        <ul className="ul-songs">
+            {/* USED IMPLICIT RETURN ON MAP */}
+          {this.props.songs.map( (song,idx) => (
+            <li className="li-songs" key={idx}>
+            <div>
+              {/* <button className="song-play-button"></button> */}
+              <span className="song-title">{song.title}</span>
+              <br />
+              <span><Link className="song-artist albumshow-artistname" to={`/artists/${song.artist.id}`}>{song.artist.name}</Link></span>
+              <span className="split-dot">.</span>
+              <span><Link className="song-album albumshow-artistname" to={`/albums/${song.album.id}`}>{song.album.title}</Link></span>
+              <span className="song-duration">{song.duration}</span>
+            </div>
+              {/* <audio className="audio-songs" controls="controls" preload="auto">
+                <source src={song.songUrl} />
+              </audio> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default SongsUser
