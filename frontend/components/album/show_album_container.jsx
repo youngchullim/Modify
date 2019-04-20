525e0f124fefa4ca31c43f36d91640c17eda9725
Young Chul Lim <youngchullim95@gmail.com>
2019-04-19 20:34:46 -0700

    Update ShowAlbumContainer

--- frontend/components/album/show_album_container.jsx
+++ frontend/components/album/show_album_container.jsx
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import ShowAlbum from './show_album';
 
 import { fetchAlbum } from '../../actions/album_actions';
+import {
+  createSongsUser,
+  deleteSongsUser,
+  createPlaylistsSong,
+  deletePlaylistsSong
+} from '../../actions/song_actions';
 
 // import { logout } from '../../actions/session_actions';
 // import { Link } from 'react-router-dom';
@@ -16,7 +22,11 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = dispatch => {
   return({
-    fetchAlbum: (id) => dispatch(fetchAlbum(id))
+    fetchAlbum: (id) => dispatch(fetchAlbum(id)),
+    createSongsUser: (user_id, song_id) => dispatch(createSongsUser(user_id, song_id)),
+    deleteSongsUser: (id) => dispatch(deleteSongsUser(id)),
+    createPlaylistsSong: (playlist_id, song_id) => dispatch(createPlaylistsSong(playlist_id, song_id)),
+    deletePlaylistsSong: (id) => dispatch(deletePlaylistsSong(id))
   });
 };
 
