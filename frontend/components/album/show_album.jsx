83f75b458a01f7f93ae3a69a0461dfd7920df774
Young Chul Lim <youngchullim95@gmail.com>
2019-04-19 20:35:19 -0700

    Comment Updated ShowAlbumComponent

--- frontend/components/album/show_album.jsx
+++ frontend/components/album/show_album.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { button, Link, NavLink } from 'react-router-dom';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 
 
 class ShowAlbum extends React.Component {
@@ -7,6 +8,7 @@ class ShowAlbum extends React.Component {
     super(props);
     
     this.toggleLibrary = this.toggleLibrary.bind(this);
+    // this.handleMenuClick = this.handleMenuClick.bind(this);
   }
 
   componentDidMount() {
@@ -18,6 +20,14 @@ class ShowAlbum extends React.Component {
     document.getElementById("album-library").classList.toggle('album-library-add');
   }
 
+  // handleMenuClick(e, data) {
+  //   // console.log(data.foo);
+  //   if (data.foo === "Add to Library") {
+  //     // this.props.createSongsUser(this.props.user.id);
+  //     console.log(this.props.user.id);
+  //     console.log(e.currentTarget.data.id);
+  //   }
+  // }
   render() {
     let albumLibrary = "REMOVE FROM YOUR LIBRARY";
     // if (!library.includes(album.title)) {
@@ -57,10 +67,25 @@ class ShowAlbum extends React.Component {
                 {/* <img className="white-play2" src={window.whitePlay2} /> */}
                 <div className="albumshow-songtitle">{song.title}</div>
                 <span className="song-duration">{song.duration}</span>
+                {/* <ContextMenuTrigger id="song-menu">
+                  <div className="album-playlist">...</div>
+                </ContextMenuTrigger> */}
               </li>
             ))}
           </ul>
         </div>
+        {/* <ContextMenu id="song-menu">
+          <MenuItem data={{foo: 'Add to Library'}} onClick={this.handleMenuClick}>
+            Add Song to Library
+          </MenuItem>
+          <MenuItem data={{foo: 'Add to Playlist'}} onClick={this.handleMenuClick}>
+            Add Song to Playlist
+          </MenuItem>
+          <MenuItem divider />
+          <MenuItem data={{foo: 'bar'}} onClick={this.handleMenuClick}>
+            ContextMenu Item 3
+          </MenuItem>
+        </ContextMenu> */}
       </div>
     )
   }
