2e87a10a302a0f6deb9f87dee53c45d53b60bfae
Young Chul Lim <youngchullim95@gmail.com>
2019-04-21 19:08:35 -0700

    Add dropdown menu for song

--- frontend/components/song/songs_user.jsx
+++ frontend/components/song/songs_user.jsx
@@ -5,15 +5,71 @@ class SongsUser extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      currentSong: ""
+    };
+    
+    this.songDropdown = this.songDropdown.bind(this);
+    this.closeDropdown = this.closeDropdown.bind(this);
+    this.removeSong = this.removeSong.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchSongsUsers(this.props.user.id);
   }
+  
+  // DROPDOWN ONLY WORKS FOR THE FIRST DROPDOWN
+  songDropdown(e) {
+    // console.log(this.state.currentSong);
+    let songName = e.target.id + 1;
+    this.setState({
+      currentSong: e.currentTarget.id
+    });
+    // console.log(songName);
+    document.getElementById(songName).classList.toggle("show");
+  }
+
+  // REMOVE NOT WORKING
+  removeSong(e) {
+    let songId = e.target.id;
+    let song = this.props.songs.filter(song => song.id === songId)
+    let userId = this.props.user.id;
+    // let users = song.songsUsers;
+    // let currentUser = users.filter(user => user.id === userId)[0];
+    console.log(song);
+    // this.props.deleteSongsUser();
+  }
+
+  closeDropdown(e) {
+    let songName = e.target.id;
+// @@@@@@ TEST @@@@@@@@
+    console.log(songName);
+    console.log("$$$$$$$$");
+    console.log(this.state.currentSong);
+    console.log("@@@@@@@@");
+    
+    if (!e.target.matches('.dropdown-button')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      for (let i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    // } else if (songName !== this.state.currentSong) {
+    //   let dropdowns = document.getElementsByClassName("dropdown-content");
+    //   for (let i = 0; i < dropdowns.length; i++) {
+    //     let openDropdown = dropdowns[i];
+    //     if (openDropdown.classList.contains('show')) {
+    //       openDropdown.classList.remove('show');
+    //     }
+    //   }
+    }
+  }
 
   render() {
     return(
-      <div className="songs-component">
+      <div className="songs-component" onClick={this.closeDropdown}>
         <ul className="ul-songs">
             {/* USED IMPLICIT RETURN ON MAP */}
           {this.props.songs.map( (song,idx) => (
@@ -25,6 +81,15 @@ class SongsUser extends React.Component {
               <span><Link className="song-artist albumshow-artistname" to={`/artists/${song.artist.id}`}>{song.artist.name}</Link></span>
               <span className="split-dot">.</span>
               <span><Link className="song-album albumshow-artistname" to={`/albums/${song.album.id}`}>{song.album.title}</Link></span>
+              
+              <div className="song-dropdown">
+                <button id={song.title} className="dropdown-button" onClick={this.songDropdown}>...</button>
+                <div id={song.title + 1} className="dropdown-content">
+                  <a id={song.id} onClick={this.removeSong}>Remove from Your Library</a>
+                  <a>Add to Playlist</a>
+                </div>
+              </div>
+              
               <span className="song-duration">{song.duration}</span>
             </div>
               {/* <audio className="audio-songs" controls="controls" preload="auto">
