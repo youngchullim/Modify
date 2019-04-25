159e685afe74aa1f34f7c275fc87fd6845bb3477
Young Chul Lim <youngchullim95@gmail.com>
2019-04-24 18:31:17 -0700

    Add commented example to how pass in props directly to a component

--- frontend/components/album/albums_user.jsx
+++ frontend/components/album/albums_user.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { button, Link, NavLink } from 'react-router-dom';
-
+import ShowAlbumContainer from './show_album_container';
+import { AuthRoute, ProtectedRoute } from '../../util/route_util';
 
 class AlbumsUser extends React.Component {
   constructor(props) {
@@ -16,11 +17,13 @@ class AlbumsUser extends React.Component {
     return(
       <div className="album-component">
         {/* <h1>Albums</h1> */}
-
+{/* example how to send in props with a component */}
+{/* <Route exact path="/mens" render={(props) => <Section {...props} gender={'Mens'}/> }/> */}
         <ul className="ul-albums">
           {this.props.albums.map( (album, idx) => (
             <li className="li-albums" key={idx}>
             <NavLink to={`/albums/${album.id}`}>
+{/* <ProtectedRoute path="/albums/:id" render={(props) => <ShowAlbumContainer {...props} album={album}/> }/> */}
               <img className="album-photo" src={album.photo} />
               <div className="album-name">{album.title}</div>
             </NavLink>
