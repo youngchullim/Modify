c7e00361b0e75564530c01af8de49969546b9971
Young Chul Lim <youngchullim95@gmail.com>
2019-05-02 12:46:05 -0700

    Add props to CreatePlaylistsSong Modal

--- frontend/components/playlist/create_playlists_song_container.jsx
+++ frontend/components/playlist/create_playlists_song_container.jsx
@@ -4,13 +4,15 @@ import CreatePlaylistsSong from './create_playlists_song';
 
 import { closeModal, openModal } from '../../actions/modal_actions';
 import { createPlaylist, fetchPlaylists } from '../../actions/playlist_actions';
+import { createPlaylistsSong } from '../../actions/song_actions';
 
 const mapStateToProps = state => {
   return({
     user: state.entities.users[state.session.id],
     formType: "Add",
-    modal:state.ui.modal,
+    modal: state.ui.modal,
     playlists: Object.values(state.entities.playlists),
+    // song: state.entities.songs[state.song],
   });
 };
 
@@ -26,6 +28,7 @@ const mapDispatchToProps = dispatch => {
     closeModal: () => dispatch(closeModal()),
     createPlaylist: (playlist) => dispatch(createPlaylist(playlist)),
     fetchPlaylists: () => dispatch(fetchPlaylists()),
+    createPlaylistsSong: (playlist_id, song_id) => dispatch(createPlaylistsSong(playlist_id, song_id)),
   });
 };
 
