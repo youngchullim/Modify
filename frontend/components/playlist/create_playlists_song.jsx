7b7d7b21d3333c969fa571cc6d180a8bad1e51a2
Young Chul Lim <youngchullim95@gmail.com>
2019-05-03 17:07:53 -0700

    Add addSong to createPlaylistsSong component

--- frontend/components/playlist/create_playlists_song.jsx
+++ frontend/components/playlist/create_playlists_song.jsx
@@ -10,10 +10,20 @@ class CreatePlaylistsSong extends React.Component {
     // };
 
     // this.handleSubmit = this.handleSubmit.bind(this);
+    this.addSong = this.addSong.bind(this);
   }
   
   componentDidMount() {
     this.props.fetchPlaylists();
+  }
+
+  addSong(e) {
+    let playlistId = e.currentTarget.id;
+    let songId = this.props.songId;
+    // console.log(playlistId);
+    // console.log(e.currentTarget);
+    // console.log(this.props.songId);
+    this.props.createPlaylistsSong(playlistId, songId);
   }
 
   // handleSubmit(e) {
@@ -39,14 +49,14 @@ class CreatePlaylistsSong extends React.Component {
             <ul className="ul-playlist">
               {this.props.playlists.map ( (playlist, idx) => (
               <li key={idx} className="li-playlist">
-                {/* <NavLink to={`/playlists/${playlist.id}`}> */}
+                <div id ={playlist.id} className="add-to-playlist" onClick={this.addSong}>
                   <div className="playlist-photo"></div>
                   <div className="playlist-name">{playlist.name}</div>
-                {/* </NavLink> */}
+                </div>
               </li>))}
             </ul>
           </div>
-          
+
           <button className="playlist-cancel" onClick={this.props.closeModal}>CANCEL</button>
           <span>{this.props.openModal}</span>
         </form>
