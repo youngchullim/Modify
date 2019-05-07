a0cc7a73252a01ed0b3f5817dcc05abb881eac06
Young Chul Lim <youngchullim95@gmail.com>
2019-05-07 06:22:27 -0700

    Update filepath of ArtistsUser

--- frontend/components/artist/artists_user_container.jsx
+++ frontend/components/artist/artists_user_container.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import ArtistsUser from './artist';
+import ArtistsUser from './artists_user';
 import { 
   fetchArtists,
   fetchArtist,
