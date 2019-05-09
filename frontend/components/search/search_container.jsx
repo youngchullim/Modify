a75b290c6867fa2a53aa11ae6586e748c33cb7d8
Young Chul Lim <youngchullim95@gmail.com>
2019-05-09 06:55:49 -0700

    Add fetch props to search Container

--- frontend/components/search/search_container.jsx
+++ frontend/components/search/search_container.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 
+import { fetchSongs } from '../../actions/song_actions';
+import { fetchArtists } from '../../actions/artist_actions';
+import { fetchAlbums } from '../../actions/album_actions';
+
 const mapStateToProps = state => {
   return({
     user: state.entities.users[state.session.id]
@@ -12,7 +16,11 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return({
-    logout: () => dispatch(logout())
+    logout: () => dispatch(logout()),
+    fetchSongs: (queries) => dispatch(fetchSongs(queries)),
+    fetchArtists: (queries) => dispatch(fetchArtists(queries)),
+    fetchAlbums: (queries) => dispatch(fetchAlbums(queries)),
+
   });
 };
 
