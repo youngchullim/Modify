b6119178b8e92f23ce6b8f0d75271a895978abba
Young Chul Lim <youngchullim95@gmail.com>
2019-05-09 15:06:26 -0700

    Add searchArtists Component

---
+++ frontend/components/search/search_artists.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { button, Link, NavLink } from 'react-router-dom';
+
+class SearchArtists extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    let result;
+    let artists = this.props.artists.filter(artist => artist.name.toLowerCase().includes(this.props.searchBar.toLowerCase()));
+
+    if (this.props.searchBar !== "") {
+      result = (
+        <div className="search-artist-component">
+          <ul className="ul-albums search-artists">
+            {artists.map( (artist, idx) => (
+              <li className="li-albums" key={idx}>
+              <NavLink to={`/artists/${artist.id}`}>
+                <img className="artist-photo" src={artist.photo} />
+                <div className="album-name">{artist.name}</div>
+              </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    return(
+      <div>
+        {result}
+      </div>
+    )
+  }
+}
+
+export default SearchArtists
