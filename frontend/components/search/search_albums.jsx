96464d5393458509ec370dccac0754ac2a3e128c
Young Chul Lim <youngchullim95@gmail.com>
2019-05-09 15:19:49 -0700

    Add searchAlbums Component

---
+++ frontend/components/search/search_albums.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { button, Link, NavLink } from 'react-router-dom';
+
+class SearchAlbums extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    let result;
+    let albums = this.props.albums.filter(album => album.title.toLowerCase().includes(this.props.searchBar.toLowerCase()));
+
+    if (this.props.searchBar !== "") {
+      result = (
+        <div className="search-artist-component">
+          <ul className="ul-albums search-artists">
+            {albums.map( (album, idx) => (
+              <li className="li-albums" key={idx}>
+              <NavLink to={`/albums/${album.id}`}>
+                <img className="album-photo" src={album.photo} />
+                <div className="album-name">{album.title}</div>
+              </NavLink>
+                <div><Link className="album-artist albumshow-artistname" to={`/artists/${album.artist.id}`}>{album.artist.name}</Link></div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    return(
+      <div>
+        {result}
+      </div>
+    )
+  }
+}
+
+export default SearchAlbums
