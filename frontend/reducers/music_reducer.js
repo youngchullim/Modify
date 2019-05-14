1678976d87c29b6ca28d24343bd43d8420165b31
Young Chul Lim <youngchullim95@gmail.com>
2019-05-14 15:16:11 -0700

    Update musicReducer testsing skeleton

--- frontend/reducers/music_reducer.js
+++ frontend/reducers/music_reducer.js
@@ -2,16 +2,18 @@ import {
   RECEIVE_CURRENT_SONG,
   RECEIVE_NEXT_SONG,
   RECEIVE_PREV_SONG,
-  RECEIVE_PLAY
+  RECEIVE_PLAY,
+  RECEIVE_SONGS_QUEUE,
 } from '../actions/music_actions';
 
 import merge from 'lodash/merge';
 
 const nullState = {
   currentSong: null,
-  currentPlaylist: null,
+  songsQueue: null,
   nextSong: null,
-  prevSong: null
+  prevSong: null,
+  play: false
 };
 
 const musicReducer = (oldState=nullState, action) => {
@@ -21,13 +23,27 @@ const musicReducer = (oldState=nullState, action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_SONG:
       state.currentSong = action.song;
+      state.nextSong = action.next;
+      state.prevSong = action.prev;
       return state;
     case RECEIVE_NEXT_SONG:
- 
+      state.currentSong = action.song;
+      state.nextSong = action.next;
+      state.prevSong = action.prev;
+      return state;
     case RECEIVE_PREV_SONG:
-
+      state.currentSong = action.song;
+      state.nextSong = action.next;
+      state.prevSong = action.prev;
+      return state;
     case RECEIVE_PLAY:
-
+      state.currentSong = action.song;
+      state.play = !state.play;
+      state.songsQueue = action.songs;
+      return state;
+    case RECEIVE_SONGS_QUEUE:
+      state.songsQueue = action.songs;
+      return state;
     default:
       return oldState;
   }
