072430ca806500e1b3c85ec631cb79f6e6589ef3
Young Chul Lim <youngchullim95@gmail.com>
2019-05-25 15:40:47 -0700

    Update navbar tags to use flex CSS

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -23,34 +23,36 @@ class Navbar extends React.Component {
 
         {/* NAV BAR */}
         <ul className="side-nav-bar">
-          <li className="nav-logo">
-                {/* USED !IMPORTANT CSS RULE */}
-            <NavLink className="logo-button" to="/home"> {/* logo button goes to HOME */}
-              <img className="white-logo" src={window.whiteLogo} />
-              <span className="white-modify">Modify</span>
-            </NavLink>
-          </li>
-          <li className="nav-home nav-tab">
-                {/* USED !IMPORTANT CSS RULE */}
-            <NavLink className="home-button button" activeClassName="selected-tab green-selected" to="/home"> 
-              <img className="white-home-logo tab-icons" src={window.home}/>     
-              <span className="white-home tabs">Home</span>
-            </NavLink>
-          </li>
-          <li className="nav-search nav-tab">
-                {/* USED !IMPORTANT CSS RULE */}
-            <NavLink className="search-button button"  activeClassName="selected-tab green-selected" to="/search">
-              <img className="white-search-logo tab-icons" src={window.search}/>      
-              <span className="white-search tabs">Search</span>
-            </NavLink>
-          </li>
-          <li className="nav-library nav-tab">
-                {/* USED !IMPORTANT CSS RULE */}
-            <NavLink className="library-button button"  activeClassName="selected-tab green-selected" to="/library"> 
-              <img className="white-library-logo tab-icons" src={window.library}/>     
-              <span className="white-library tabs">Your Library</span>
-            </NavLink>
-          </li>
+          <div className="navbar-flex">
+            <li className="nav-logo">
+                  {/* USED !IMPORTANT CSS RULE */}
+              <NavLink className="logo-button" to="/home"> {/* logo button goes to HOME */}
+                <img className="white-logo" src={window.whiteLogo} />
+                <span className="white-modify">Modify</span>
+              </NavLink>
+            </li>
+            <li className="nav-home nav-tab">
+                  {/* USED !IMPORTANT CSS RULE */}
+              <NavLink className="home-button button" activeClassName="selected-tab green-selected" to="/home"> 
+                <img className="white-home-logo tab-icons" src={window.home}/>     
+                <span className="white-home tabs">Home</span>
+              </NavLink>
+            </li>
+            <li className="nav-search nav-tab">
+                  {/* USED !IMPORTANT CSS RULE */}
+              <NavLink className="search-button button"  activeClassName="selected-tab green-selected" to="/search">
+                <img className="white-search-logo tab-icons" src={window.search}/>      
+                <span className="white-search tabs">Search</span>
+              </NavLink>
+            </li>
+            <li className="nav-library nav-tab">
+                  {/* USED !IMPORTANT CSS RULE */}
+              <NavLink className="library-button button"  activeClassName="selected-tab green-selected" to="/library"> 
+                <img className="white-library-logo tab-icons" src={window.library}/>     
+                <span className="white-library tabs">Your Library</span>
+              </NavLink>
+            </li>
+          </div>
           <li className="nav-profile">
                 {/* USED !IMPORTANT CSS RULE */}
             <NavLink className="profile-button"  activeClassName="selected-profile" to="/profile">
