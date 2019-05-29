dd42d540e65485d9421a5e634c32daa535948139
Young Chul Lim <youngchullim95@gmail.com>
2019-05-28 17:19:33 -0700

    Add basic music Component skeleton

--- frontend/components/music/music.jsx
+++ frontend/components/music/music.jsx
@@ -15,22 +15,34 @@ class Music extends React.Component {
       play: false,
       pause: true,
       duration: this.props.currentSong ? this.props.currentSong.duration : "",
-      currentTime: this.props.currentSong ? "0:00" : "",
+      // currentTime: this.props.currentSong ? "0" : "",
+      currentTime: 0,
       songsQueue: this.props.songsQueue,
       volume: 0.6,
+      preVolume: 0.6,
       muted: false,
+      seeking: false,
     };
     
     this.handlePlay = this.handlePlay.bind(this);
     this.handlePrev = this.handlePrev.bind(this);
     this.handleNext = this.handleNext.bind(this);
     this.saveSong = this.saveSong.bind(this);
+    this.handleVolume = this.handleVolume.bind(this);
+    this.handleMute = this.handleMute.bind(this);
+    this.handleSlider = this.handleSlider.bind(this);
+    this.onSeekMouseDown = this.onSeekMouseDown.bind(this);
+    this.onSeekMouseUp = this.onSeekMouseUp.bind(this);
+    this.ref = this.ref.bind(this);
+    //TEST
+    // this.onDuration = this.onDuration.bind(this);
+    // this.onProgress = this.onProgress.bind(this);
   }
   componentDidMount() {
     this.props.receiveCurrentSong(this.props.currentSong, this.props.nextSong, this.props.prevSong);
     // this.props.fetchSongsUsers(this.props.user.id);
   }
-
+  
   componentDidUpdate(prevProps) {
     if (prevProps.currentSong !== this.props.currentSong) {
       this.setState({
@@ -41,10 +53,10 @@ class Music extends React.Component {
         play: this.props.play,
         pause: this.props.pause,
         duration: this.props.currentSong ? this.props.currentSong.duration : "",
-        currentTime: this.props.currentSong ? "0:00" : "",
+        // currentTime: this.props.currentSong ? "0:00" : "",
         songsQueue: this.props.songsQueue,
-      })
-    this.props.receiveCurrentSong(this.props.currentSong, this.props.nextSong, this.props.prevSong);
+      });
+      this.props.receiveCurrentSong(this.props.currentSong, this.props.nextSong, this.props.prevSong);
     }
   }
 
@@ -117,6 +129,62 @@ class Music extends React.Component {
     this.props.createSongsUser(this.props.user.id, this.props.currentSong.id);
   }
 
+  handleVolume(e) {
+    let volume = parseFloat(e.target.value);
+    this.setState({
+      volume: volume,
+      preVolume: volume
+    });
+  }
+
+  handleMute() {
+    if (this.state.muted) {
+      this.setState({
+        muted: !this.state.muted,
+        volume: this.state.preVolume
+      });
+    } else {
+      this.setState({
+        muted: !this.state.muted,
+        volume: 0
+      });
+    }
+  }
+
+  handleSlider(e) {
+    this.setState({
+      currentTime: Math.floor(e.target.value)
+    });
+  }
+
+  onSeekMouseDown(e) {
+    this.setState({
+      seeking: true
+    });
+  }
+
+  onSeekMouseUp(e) {
+    this.setState({
+      seeking: false
+    });
+    this.player.seekTo(Math.floor(e.target.value));
+  }
+
+  ref(player) {
+    this.player = player;
+  }
+
+// TEST
+  // onDuration(duration) {
+  //   this.setState({duration});
+  // }
+
+  // onProgress(state) {
+  //   if (!this.state.seeking) {
+  //     this.setState({state});
+  //   }
+  // }
+  
   render() {
     let play;
     if (this.state.play) {
@@ -140,8 +208,8 @@ class Music extends React.Component {
     }
 
     let save;
-    // filter through user's saved songs and if its already saved then show a green checkmark 
-    // else show a plus sign
+// NOTE: filter through user's saved songs and if its already saved then show a green checkmark 
+// else show a plus sign
     if (this.state.currentSong) {
       // if (this.props.songs.includes(this.props.currentSong)) {
       //   save = (<img className="musicPlayer-plus"src={window.musicPlayerCheck} />)
@@ -153,19 +221,50 @@ class Music extends React.Component {
     }
 
 // TEST
-    // let duration;
-    // if (this.props.currentSong) {
-    //   duration = this.props.currentSong.duration;
-    // } else {
-    //   duration = "";
-    // }
-    // let dur = duration.split(":");
-    // let min = parseInt(dur[0]) * 60
-    // let sec = parseInt(dur[1]);
-    // let total = min + sec;
+    let duration;
+    if (this.props.currentSong) {
+      duration = this.props.currentSong.duration;
+    } else {
+      duration = "";
+    }
+    let dur = duration.split(":");
+    let min = parseInt(dur[0]) * 60
+    let sec = parseInt(dur[1]);
+
+    let total;
+    if (this.props.currentSong) {
+      total = min + sec;
+    } else {
+      total = "0"
+    }
+
+    let time;
+    let currentTime;
+    if (this.state.currentSong) {
+      currentTime = parseInt(this.state.currentTime);
+      let min = Math.floor(currentTime / 60);
+      let sec = currentTime % 60;
+      if (min < 10 && sec < 10) {
+        time = "0" + min + ":" + "0" + sec;
+      } else if (min < 10 && sec > 9) {
+        time = "0" + min + ":" + sec;
+      } else if (min > 10 && sec < 10) {
+        time = min + ":" + "0" + sec;
+      } else {
+        time = min + ":" + sec;
+      }
+    }
+
+    let sound;
+    if (this.state.volume === 0 || this.state.muted) {
+      sound = window.musicPlayerMute;
+    } else {
+      sound = window.musicPlayerSound;
+    }
 
     return(
       <div className="music-player-container">
+{/* MUSICPLAYER LEFT */}
         <div className="musicPlayer-flex">
           <div className="currentSong-info">
             {albumPhoto}
@@ -177,7 +276,7 @@ class Music extends React.Component {
               {save}
             </div>
           </div>
-
+{/* MUSICPLAYER CENTER */}
           <div className="musicPlayer-controls">
             <div className="musicPlayer-buttons">
               <div className="prev-button" onClick={this.handlePrev}>
@@ -192,42 +291,64 @@ class Music extends React.Component {
                 <img className="musicPlayer-prev" src={window.musicPlayerNext}/>
               </div>
             </div>
-{/* TEST */}
-            {/* <p>
-              <input ref={(slider) => { this.slider = slider }}
-              type="range"
-              name="points"
-              min="0" max={this.state.duration} /> 
-            </p> */}
 
             <div className="musicPlayer-songBar">
-              <div className="currentSong-time">{this.state.currentTime}</div>
-{/* TEST */}
-              {/* <progress 
-                pseudo='-webkit-progress-bar'
-                className='progress-bar-display' 
-                max={1} 
-                value={total} 
-                /> */}
-                <div className="currentSong-time">{this.state.duration}</div>
+              <div className="currentSong-time">{time}</div>
+
+              <div className="musicPlayer-progressBar">
+                <input 
+                  id='audio-input'
+                  className="progressBar-slider" 
+                  type="range" 
+                  min={0} 
+                  max={total} 
+                  step="any" 
+                  onChange={this.handleSlider} 
+                  value={this.state.currentTime} 
+                  onMouseDown={this.onSeekMouseDown}
+                  onMouseUp={this.onSeekMouseUp}
+                />
+                {/* <progress max={total} value={this.state.currentTime} /> */}
+                {/* <div className="outer-bar">
+                  <div className="inner-bar" style={{width: `${(this.state.currentTime*100/(this.state.duration || 1))}%`}}></div>
+                </div> */}
               </div>
+            
+              <div className="currentSong-time">{this.state.duration}</div>
+            </div>
           </div>
+{/* MUSICPLAYER RIGHT */}
           <div className="musicPlayer-volume">
-            TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
+            <div className="volume-div">
+              <div className="sound-button" onClick={this.handleMute}>
+                <img className="musicPlayer-sound" src={sound} />
+              </div>
+              <input 
+                id='valumeBar'
+                className="volumeBar-slider" 
+                type="range" 
+                min={0} 
+                max={1} 
+                step="any" 
+                onChange={this.handleVolume} 
+                value={this.state.volume} 
+              />
+            </div>
           </div>
         </div>
         <ReactPlayer
+          ref={this.ref}
           className="react-music-player"
+          id="audio"
           url={this.state.songUrl}
           playing={this.state.play}
-          // controls={true}
-          // light={true}
           onEnded={this.handleNext}
           // loop={false}
           volume={this.state.volume}
           muted={this.state.muted}
-          width='60%'
-          height='0%'
+          // onProgress={this.onProgress}
+          // onDuration={this.onDuration}
+          // progressInterval={this.state.curretTime}
         />
       </div>
     )
