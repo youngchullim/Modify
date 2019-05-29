29b2a48899530130fb1385e2c95680348d2e9530
Young Chul Lim <youngchullim95@gmail.com>
2019-05-29 12:28:42 -0700

    Add props to showArtist Container

--- frontend/components/artist/show_artist_container.jsx
+++ frontend/components/artist/show_artist_container.jsx
@@ -17,15 +17,24 @@ import {
   fetchSongsUsers
 } from '../../actions/song_actions';
 
+import { 
+  receiveCurrentSong,
+  receiveSongsQueue,
+  receivePlay,
+  receivePause,
+  fetchCurrentSong,
+  receiveCurrentSongId,
+} from '../../actions/music_actions';
+
 import { openModal, closeModal } from '../../actions/modal_actions';
-import { receiveCurrentSong } from '../../actions/music_actions';
 
 const mapStateToProps = (state, ownProps) => {
   return({
     user: state.entities.users[state.session.id],
     artist: state.entities.artists[ownProps.match.params.id],
-    artists: state.entities.artists
-
+    artists: state.entities.artists,
+    songs: Object.values(state.entities.songs),
+    song: Object.values(state.entities.songs)[0],
     // DOESNT WORK
     // albums: state.entities.artists[ownProps.match.params.id].ablums,
     // songs: state.entities.artists[ownProps.match.params.id].songs,
@@ -42,13 +51,19 @@ const mapDispatchToProps = dispatch => {
     
     openModal: (modal,songId) => dispatch(openModal(modal,songId)),
     closeModal: () => dispatch(closeModal()),
-    receiveCurrentSong: (song) => dispatch(receiveCurrentSong(song)),
 
     createSongsUser: (user_id, song_id) => dispatch(createSongsUser(user_id, song_id)),
     deleteSongsUser: (id) => dispatch(deleteSongsUser(id)),
     createPlaylistsSong: (playlist_id, song_id) => dispatch(createPlaylistsSong(playlist_id, song_id)),
     // deletePlaylistsSong: (id) => dispatch(deletePlaylistsSong(id)),
     // fetchSongsUsers: (id) => dispatch(fetchSongsUsers(id)),
+
+    fetchCurrentSong: (userId, id) => (dispatch(fetchCurrentSong(userId, id))),
+    receivePlay: (song, songs) => (dispatch(receivePlay(song, songs))),
+    receivePause: (song, songs) => (dispatch(receivePause(song, songs))),
+    receiveSongsQueue: (songs) => dispatch(receiveSongsQueue(songs)),
+    receiveCurrentSong: (song, next, prev) => dispatch(receiveCurrentSong(song, next, prev)),
+    receiveCurrentSongId: (songId) => dispatch(receiveCurrentSongId(songId)),
   });
 };
 
