868bd03c7eb01cf984ce93bf6c6d45612dd7325a
Young Chul Lim <youngchullim95@gmail.com>
2019-06-11 07:23:06 -0700

    Add currentSong to searchResult Container

--- frontend/components/search/search_result_container.jsx
+++ frontend/components/search/search_result_container.jsx
@@ -34,6 +34,7 @@ const mapStateToProps = (state, ownProps) => {
     songs: Object.values(state.entities.songs),
     song: Object.values(state.entities.songs)[0],
     searchBar: ownProps.queries,
+    currentSong: state.ui.music.currentSong,
   });
 };
 
