13e822de9e3725de84cc20dd45ace184a75ecaf4
Young Chul Lim <youngchullim95@gmail.com>
2019-06-11 07:57:40 -0700

    Test production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,16 +83,19 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+# TEST to remove logs in production
+  # if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #   logger           = ActiveSupport::Logger.new(STDOUT)
+  #   logger.formatter = config.log_formatter
+  #   config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  # end
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger           = nil
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = nil
   end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # TEST to remove logs in production
-  config.active_record.logger = nil
-  
 end
