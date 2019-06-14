a87892a7a5caa99eb780924ffc4e35fab9e52550
Young Chul Lim <youngchullim95@gmail.com>
2019-06-14 16:45:18 -0700

    Add ShowGenre Container

--- frontend/components/auth.jsx
+++ frontend/components/auth.jsx
@@ -17,7 +17,7 @@ import ProfileContainer from './profile/profile_container';
 import ShowAlbumContainer from './album/show_album_container';
 import ShowArtistContainer from './artist/show_artist_container';
 import ShowPlaylistContainer from './playlist/show_playlist_container';
-
+import ShowGenreContainer from './genre/show_genre_container';
 // import ShowPlaylistContainer from './playlist/show_playlist_container';
 
 import { Route, Switch } from 'react-router-dom';
@@ -39,6 +39,7 @@ const AuthComponent = () => {
           <ProtectedRoute path="/albums/:id" component={ShowAlbumContainer} />
           <ProtectedRoute path="/artists/:id" component={ShowArtistContainer} />
           <ProtectedRoute path="/playlists/:id" component={ShowPlaylistContainer} />
+          <ProtectedRoute path="/genre/:title" component={ShowGenreContainer} />
           <ProtectedRoute path="/" component={HomeContainer} />
         </Switch>
       </div>
