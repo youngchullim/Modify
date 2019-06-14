9a8660cb563de8977eca8e6b4c2a564cb6d26bb8
Young Chul Lim <youngchullim95@gmail.com>
2019-06-14 16:45:53 -0700

    Add index genre Component

--- frontend/components/genre/genre.jsx
+++ frontend/components/genre/genre.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import { button, Link, NavLink } from 'react-router-dom';
 
 class Genre extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      genre: [],
+      songs: this.props.songs,
+    };
+
+  }
+
+  componentDidMount() {
+    this.props.fetchSongs();
+  }
+
   render() {
+    let genre = this.props.songs.filter (song => song.genre);
+    for (let i  = 0; i < genre.length; i++) {
+      if (!this.state.genre.includes(genre[i].genre)) {
+        this.state.genre.push(genre[i].genre);
+      }
+    }
     return(
-      <div>
-        <h1>Genre 1</h1>
+      <div className="all-playlists">
+        {/* <h1>Genre 1</h1> */}
+        <ul className="ul-playlist">
+                  {/* ADD implicit return () */}
+          {this.state.genre.map ( (genre, idx) => (
+          <li key={idx} className="li-playlist">
+            <NavLink to={`/genre/${genre}`}>
+              <div className="playlist-photo"></div>
+              <div className="playlist-name">{genre}</div>
+            </NavLink>
+          </li>))}
+        </ul>
       </div>
     )
   }
--- frontend/components/genre/genre_container.jsx
+++ frontend/components/genre/genre_container.jsx
@@ -3,16 +3,19 @@ import { logout } from '../../actions/session_actions';
 import Genre from './genre';
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { fetchSongs } from '../../actions/song_actions';
 
 const mapStateToProps = state => {
   return({
-    user: state.entities.users[state.session.id]
+    user: state.entities.users[state.session.id],
+    songs: Object.values(state.entities.songs),
   });
 };
 
 const mapDispatchToProps = dispatch => {
   return({
-    logout: () => dispatch(logout())
+    logout: () => dispatch(logout()),
+    fetchSongs: () => dispatch(fetchSongs()),
   });
 };
 
